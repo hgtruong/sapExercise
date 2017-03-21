acd6bd402e295326f8adf9891d31a353b9fecff7
Huy Truong <HT@huys-mbp.attlocal.net>
2017-03-21 08:00:35 -0700

    Initial Commit

---
+++ sapExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sapExercise
+//
+//  Created by Huy Truong on 3/21/17.
+//  Copyright © 2017 Huy Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
